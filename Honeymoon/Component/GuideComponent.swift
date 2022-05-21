cd98a0474b16920a5910e9e74e1027a82afffedd
Aman Sharma <shrma.aman7@gmail.com>
2022-05-21 18:03:56 +0530

    component complete

---
+++ Honeymoon/Component/GuideComponent.swift
@@ -0,0 +1,55 @@
+//
+//  GuideComponent.swift
+//  Honeymoon
+//
+//  Created by AMAN SHARMA on 21/05/22.
+//
+
+import SwiftUI
+
+struct GuideComponent: View {
+  // MARK: - PROPERTIES
+  var title: String
+  var subTitle: String
+  var description: String
+  var icon: String
+  // MARK: - BODY
+    var body: some View {
+      HStack(alignment: .center, spacing: 20) {
+        Image(systemName: icon)
+          .font(.largeTitle)
+          .foregroundColor(Color.pink)
+        
+        VStack(alignment: .leading, spacing: 4) {
+          HStack{
+            Text(title.uppercased())
+              .font(.title)
+              .fontWeight(.heavy)
+            
+            Spacer()
+            
+            Text(subTitle.uppercased())
+              .font(.footnote)
+              .fontWeight(.heavy)
+              .foregroundColor(Color.pink)
+          } //: HSTACK
+          
+          Divider().padding(.bottom, 4)
+          
+          Text(description)
+            .font(.footnote)
+            .foregroundColor(Color.secondary)
+            .fixedSize(horizontal: false, vertical: true)
+
+        } //: VSTACK
+      } //: HSTACK
+    }
+}
+
+  // MARK: - PREVIEW
+struct GuideComponent_Previews: PreviewProvider {
+    static var previews: some View {
+      GuideComponent(title: "Title", subTitle: "Swipe right", description: "This is a placeholder sentence. This is a placeholder sentence. This is a placeholder sentence.", icon: "heart.circle")
+        .previewLayout(.sizeThatFits)
+    }
+}
