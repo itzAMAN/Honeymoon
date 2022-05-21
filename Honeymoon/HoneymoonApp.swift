27fd17b918ffb889040eb480db251b7387a859d9
Aman Sharma <shrma.aman7@gmail.com>
2022-05-21 16:12:51 +0530

    Initial Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by AMAN SHARMA on 21/05/22.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
